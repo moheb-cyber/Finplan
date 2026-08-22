08d899687639a9d27bf5eee64dc44e367976781a
Reza <mohebhosseinifard@email.com>
2026-08-22 23:32:04 +0330

    feat: add database and transaction model

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.sql import func
+
+from backend.database import Base
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    title = Column(String, nullable=False)
+
+    amount = Column(Float, nullable=False)
+
+    type = Column(String, nullable=False)
+
+    category = Column(String, nullable=False)
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
